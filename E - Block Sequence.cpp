82372a6372deb580fe5b12998a7122ae2b5340f8
Mohamed Yassine Landolsi <157914237+Ylandolsi@users.noreply.github.com>
2024-03-09 11:31:26 +0100

    E - Block Sequence.cpp

---
+++ E - Block Sequence.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define pb push_back
+#define SPEED cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0)
+typedef long long ll ;
+typedef pair<ll,pair<ll,ll>> tup;
+typedef pair<ll,ll> pi;
+#ifndef ONLINE_JUDGE
+#include "debug.cpp"
+#endif
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define sortt(x) sort(all(x))
+#define getunique(vt)  vt.erase(unique(all(vt)), vt.end())
+#define rev(x)   reverse (all(x)) 
+#define  bts(x) __builtin_clz(x)
+ll modInverse(ll a , ll modd) {return a <= 1 ? a : modd - (ll)(modd/a) * modInverse(modd % a , modd) % modd;}
+bool sortinc(pi &a,pi &b){ if ( a.second == b.second ) return a.first < b.first ;  else  (a.second < b.second);}
+bool sortdec(pi &a,pi &b){ if ( a.second == b.second ) return a.first > b.first ;  else  (a.second > b.second);}
+ll mceil( ll a , ll b ) { return (a+b-1)/b ; }
+template<class T> bool ckmax(T& a, const T& b){return a < b ? a = b, 1 : 0;}
+template<class T> bool ckmin(T& a, const T& b){return a > b ? a = b, 1 : 0;}
+
+class Solution {
+public:
+     int n ;
+     void solve(){
+          cin >> n ;
+          vector<int>a(n) , dp(n+3 , 0 ) ;
+
+          for(auto &d : a )  cin>>d;
+          for ( int i = n - 1 ; i >= 0 ; i-- ) {
+               if ( i + a[i] <= n-1 ) {
+                    dp[i] = dp[i+a[i]+1] + a[i] +1   ;
+               }
+               dp[i]=max(dp[i+1],dp[i]);
+          }
+          cout << n - (*max_element(all(dp))) <<endl ;
+               
+
+               
+     }
+     
+};
+
+
+
+int32_t main() {
+     cout << fixed << setprecision(2) ;
+     SPEED;
+     #ifndef ONLINE_JUDGE
+        freopen("inp.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+        freopen("err.txt", "w", stderr);
+     #endif
+     int t = 1;
+     cin >> t;
+     while (t--){
+          Solution sol;  
+          sol.solve();
+     } 
+    return 0;
+}
