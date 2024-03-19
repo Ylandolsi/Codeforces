c56fbeb86ee85dc3bc7392187995908fca212b6c
Mohamed Yassine Landolsi <157914237+Ylandolsi@users.noreply.github.com>
2024-03-19 13:37:26 +0100

    E - Binary Search.cpp

---
+++ E - Binary Search.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std ; 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define pb push_back
+#define i_love_binary_search  cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0) ; 
+typedef long long ll ;
+typedef pair<ll,pair<ll,ll>> tup;
+typedef pair<ll,ll> pi;
+#ifndef ONLINE_JUDGE
+#include "debug.cpp"
+#endif
+#define all(x) x.begin(), x.end()
+#define sortt(x) sort(all(x))
+#define getunique(vt)  vt.erase(unique(all(vt)), vt.end())
+#define rev(x)   reverse (all(x)) 
+#define  ibits(x) 32 - __builtin_clz(x) // 32 - bts(x) if int 
+#define  lbits(x) 64 -  __builtin_clzll(x) // 64 - bts(x) if long long 
+ll modInverse(ll a , ll modd) {return a <= 1 ? a : modd - (ll)(modd/a) * modInverse(modd % a , modd) % modd;}
+bool sortinc(pi &a,pi &b){ if ( a.second == b.second ) return a.first < b.first ;  else  (a.second < b.second);}
+bool sortdec(pi &a,pi &b){ if ( a.second == b.second ) return a.first > b.first ;  else  (a.second > b.second);}
+ll mceil( ll a , ll b ) { return (a+b-1)/b ; }
+template<typename... T>void read(T&... args) {((cin >> args), ...);}
+template<typename... T>void write(T&&... args) {((cout << args /*<< " "*/), ...);}
+template<class T> bool ckmax(T& a, const T& b){return a < b ? a = b, 1 : 0;}
+template<class T> bool ckmin(T& a, const T& b){return a > b ? a = b, 1 : 0;}
+template<typename F, typename S>ostream& operator <<(ostream& ostream, pair<F, S>& p) {cout << p.first << " " << p.second;return ostream;}
+template<typename T>ostream& operator <<(ostream& ostream, vector<T>& v) {for(auto& element : v) {cout << element << " ";}return ostream;}
+template<typename T>ostream& operator <<(ostream& ostream, vector<vector<T>>& v) {for(auto& row : v) {for(auto& cell : row) {cout << cell << " ";}cout << "\n";}return ostream;}
+ordered_set<ll> st;
+
+
+class Solution {
+public:
+     int n , k , indexofK   ;
+     void Test_Case(){
+          read( n , k ) ; 
+          int r = n+1, l =  1 ;
+          vector<int>vect(n) , deletions ;
+          for(auto &d : vect )  cin>>d;
+          set<int>indexExclus , indexofelsmallerthanK ; 
+          for ( int i = 0 ; i < n ; i++ ){
+               indexExclus.insert(i);
+               if ( vect[i] < k ) indexofelsmallerthanK.insert(i);
+               
+          }
+          for ( int i = 0 ; i < n ; i++ ){
+               if ( vect[i] == k ){ indexofK = i ;  break ;}
+          }
+          
+          while ( r - l > 1  ){
+               int mid = (l+r)/2 ;
+               indexExclus.erase(mid-1);
+               if ( vect[mid-1] < k ) deletions.pb(mid-1);
+               if ( vect[mid-1] <= k ) l = mid ;
+               else r = mid ; 
+          }
+
+          if  ( deletions.size() ){
+               for ( int i = 0 ; i < deletions.size() -1  ; i++ )
+                    indexofelsmallerthanK.erase(deletions[i]);
+               
+          }
+          if ( vect[l-1] == k ){
+               write( 0 , "\n");
+               return ;
+          }
+          if ( indexExclus.find(indexofK) != indexExclus.end()) 
+               write ( 1 , "\n" , indexofK + 1  , " " , l  , "\n");
+          
+          else {
+               int x = *indexofelsmallerthanK.begin() + 1 ; 
+               write (  2 , "\n", x , " " , indexofK + 1  , "\n" , x  , " " , l  , "\n");
+
+          }
+
+      
+     }
+     
+};
+
+
+
+int32_t main() {
+     cout << fixed << setprecision(2) ;
+     i_love_binary_search 
+     #ifndef ONLINE_JUDGE
+        freopen("inp.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+        freopen("err.txt", "w", stderr);
+     #endif
+     int t = 1;
+     cin >> t;
+     while (t--){
+          Solution sol;  
+          sol.Test_Case();
+     } 
+    return 0;
+}
